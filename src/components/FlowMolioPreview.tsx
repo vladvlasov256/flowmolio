72311306993f48860106de91d5f00ceaa0f57cd4
Vladimir Vlasov <crivlaldo@gmail.com>
2025-05-04 11:15:16 +0200

    feat: convert preview from div to svg

--- src/components/FlowMolioPreview.tsx
+++ src/components/FlowMolioPreview.tsx
@@ -1,22 +1,30 @@
-import React, { forwardRef, useMemo } from 'react';
+import parse from 'html-react-parser';
+import React, { useMemo } from 'react';
 
 import { PreviewObject } from '../types';
 import { renderFlowMolio } from '../utils/renderFlowMolio';
 
-interface FlowMolioPreviewProps extends React.HTMLAttributes<HTMLDivElement> {
+interface FlowMolioPreviewProps extends React.SVGProps<SVGSVGElement> {
   previewObject: PreviewObject;
   dataSources: any;
 }
 
-export const FlowMolioPreview = forwardRef<HTMLDivElement, FlowMolioPreviewProps>(({ previewObject, dataSources, ...divProps }, ref) => {
-    const renderedSvg = useMemo(() => {
+export const FlowMolioPreview: React.FC<FlowMolioPreviewProps> = ({ previewObject, dataSources, ...svgProps }) => {
+  const renderedSvg = useMemo(() => {
     if (previewObject && dataSources) {
       return renderFlowMolio(previewObject, dataSources);
     }
     return ""
   }, [previewObject, dataSources]);
 
-  return (
-      <div ref={ref} dangerouslySetInnerHTML={{ __html: renderedSvg }} {...divProps}  />
-  );
-});
+  const svg = useMemo(() => renderedSvg ? parse(renderedSvg) : null, [renderedSvg]);
+
+  if (React.isValidElement<SVGSVGElement>(svg)) {
+    const validSvgProps = Object.fromEntries(
+      Object.entries(svgProps).filter(([key]) => key in SVGElement.prototype)
+    );
+    return React.cloneElement(svg, validSvgProps);
+  }
+
+  return <svg {...svgProps} />;
+};
